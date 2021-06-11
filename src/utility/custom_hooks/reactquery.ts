19b88be897e5787ec22c66f24b27220c7b619021
jeff-ofobrukweta <oghenerukevwejeff@gmail.com>
2021-06-11 23:17:15 +0100

    refactor: card developers

--- src/utility/custom_hooks/reactquery.ts
+++
@@ -1,9 +0,0 @@
-
-import { useQueryClient } from "react-query";
-
-const queryCli = useQueryClient();
-
-
-export default function useQueryClient(clientPrefix, data){
-    queryCli.setQueryData([`${clientPrefix}`], data);
-}
