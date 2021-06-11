033ace0853a97c4f45db21a6c78cf3e531e4b8c8
gift <geezyenyoghasim@gmail.com>
2021-06-11 14:41:35 +0100

    feat : worked on the repositories card

--- src/component/card/bg-board-card/card.tsx
+++ src/component/card/bg-board-card/card.tsx
@@ -1,48 +1,22 @@
 import { useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
-import { useQuery } from "react-query";
 import Dropdown from "../../dropdown/dropdown";
-import ListCardRepository from "../sm-list-card-repository/card";
 import { connect } from "react-redux";
 import { getEnums } from "../../../redux/thunk";
 import { HOME_ROUTE, DEVELOPERS } from "../../../routes/constant";
 import spokenLanguages from "../../../api.service/spoken_languages";
-import request from "../../../api.service/axios.factory";
 import "./card.css";
 
-const Card = ({ match, fetchEnums, getAllEnums, children }: any) => {
+const Card = (props: any) => {
+  console.log("card - props", typeof props.params.language);
+
   const useQueryParams = () => {
     return new URLSearchParams(useLocation().search);
   };
   const query = useQueryParams();
-  const func = async () => {
-    const { data } = await request(
-      "GET",
-      `${
-        match.path.includes(DEVELOPERS)
-          ? `/developers${
-              match.params.language ? `/${match.params.language}` : ""
-            }?${query.get("since") ? `&since=${query.get("since")}` : ""}${
-              query.get("spoken_language_code")
-                ? `&spoken_language_code=${query.get("spoken_language_code")}`
-                : ""
-            }`
-          : `/repositories${
-              match.params.language ? `/${match.params.language}` : ""
-            }?${query.get("since") ? `&since=${query.get("since")}` : ""}${
-              query.get("spoken_language_code")
-                ? `&spoken_language_code=${query.get("spoken_language_code")}`
-                : ""
-            }`
-      }`,
-      ""
-    );
-    return data;
-  };
-  const { data, isLoading, isError, isSuccess }: any = useQuery("dev", func);
   useEffect(() => {
     const fetcher = async () => {
-      await fetchEnums();
+      await props?.fetchEnums();
     };
     fetcher();
   }, []);
@@ -55,7 +29,7 @@ const Card = ({ match, fetchEnums, getAllEnums, children }: any) => {
             <Link
               to={HOME_ROUTE}
               className={`${
-                !match.path.includes(DEVELOPERS)
+                !props?.path?.includes(DEVELOPERS)
                   ? "active left link"
                   : "left link"
               }`}
@@ -65,7 +39,7 @@ const Card = ({ match, fetchEnums, getAllEnums, children }: any) => {
             <Link
               to={DEVELOPERS}
               className={`${
-                match.path.includes(DEVELOPERS)
+                props?.path?.includes(DEVELOPERS)
                   ? "active right link"
                   : "right link"
               }`}
@@ -74,7 +48,7 @@ const Card = ({ match, fetchEnums, getAllEnums, children }: any) => {
             </Link>
           </nav>
           <div className="dropdown-links">
-            {match?.path?.includes(DEVELOPERS) === false && (
+            {props?.path?.includes(DEVELOPERS) === false && (
               <span>
                 <Dropdown
                   type="spoken-lang"
@@ -83,7 +57,7 @@ const Card = ({ match, fetchEnums, getAllEnums, children }: any) => {
                   inputPlaceholder="Filter spoken languages"
                   flag="spoken"
                   data={spokenLanguages}
-                  proLang={match.params.language}
+                  proLang={props?.params?.language}
                   boldText={
                     spokenLanguages.filter(
                       (e) => e.urlParam === query.get("spoken_language")
@@ -103,14 +77,18 @@ const Card = ({ match, fetchEnums, getAllEnums, children }: any) => {
 
             <span>
               <Dropdown
-                match={match}
+                isExact={props.isExact}
+                params={props.params}
+                path={props.path}
                 type="pro-lang"
                 headerText="Select a language"
                 searchTitle="Language"
                 inputPlaceholder="Filter languages"
-                data={getAllEnums?.allowedProgrammingLanguages}
-                proLang={match.params.language}
-                boldText={match.params.language ? match.params.language : "Any"}
+                data={props?.getAllEnums?.allowedProgrammingLanguages}
+                proLang={props?.params?.language}
+                boldText={
+                  props?.params?.language ? props?.params?.language : "Any"
+                }
                 spokenLang={
                   query.get("spoken_language") !== null
                     ? query.get("spoken_language")
@@ -121,17 +99,19 @@ const Card = ({ match, fetchEnums, getAllEnums, children }: any) => {
             </span>
             <span>
               <Dropdown
-                match={match}
+                isExact={props.isExact}
+                params={props.params}
+                path={props.path}
                 type="date"
                 headerText="Date range"
                 searchTitle="Adjust time span"
                 inputPlaceholder="hi"
-                data={getAllEnums?.allowedDates}
+                data={props?.getAllEnums?.allowedDates}
                 withInput={false}
                 boldText={
                   query.get("since") !== null ? query.get("since") : "Any"
                 }
-                proLang={match.params.language}
+                proLang={props?.params?.language}
                 spokenLang={
                   query.get("spoken_language") !== null
                     ? query.get("spoken_language")
@@ -142,29 +122,7 @@ const Card = ({ match, fetchEnums, getAllEnums, children }: any) => {
             </span>
           </div>
         </div>
-        <main className="card-holder-body-wrapper">
-          {isLoading ? (
-            <div className="loading">
-              Loading{" "}
-              {`${
-                match.path.includes(DEVELOPERS) ? "developers" : "repositories"
-              }`}
-            </div>
-          ) : isError ? (
-            <div className="error">
-              Error Loading{" "}
-              {`${
-                match.path.includes(DEVELOPERS) ? "developers" : "repositories"
-              }`}
-            </div>
-          ) : isSuccess ? (
-            data.map((item: any, index: any) => {
-              return <ListCardRepository {...item} key={index} />;
-            })
-          ) : (
-            ""
-          )}
-        </main>
+        <main className="card-holder-body-wrapper">{props.children}</main>
       </div>
     </>
   );
--- src/page/home/home.tsx
+++ src/page/home/home.tsx
@@ -1,11 +1,72 @@
 import Card from "../../component/card/bg-board-card/card";
+import { useQuery } from "react-query";
+import ListCardRepository from "../../component/card/sm-list-card-repository/card";
+import { useLocation } from "react-router";
+import request from "../../api.service/axios.factory";
+import { DEVELOPERS } from "../../routes/constant";
 import "./home.css";
+
 const Home = ({ match }: any) => {
+  const useQueryParams = () => {
+    return new URLSearchParams(useLocation().search);
+  };
+  const query = useQueryParams();
+  const func = async () => {
+    const { data } = await request(
+      "GET",
+      `${
+        match.path.includes(DEVELOPERS)
+          ? `/developers${
+              match.params.language ? `/${match.params.language}` : ""
+            }?${query.get("since") ? `&since=${query.get("since")}` : ""}${
+              query.get("spoken_language_code")
+                ? `&spoken_language_code=${query.get("spoken_language_code")}`
+                : ""
+            }`
+          : `/repositories${
+              match.params.language ? `/${match.params.language}` : ""
+            }?${query.get("since") ? `&since=${query.get("since")}` : ""}${
+              query.get("spoken_language_code")
+                ? `&spoken_language_code=${query.get("spoken_language_code")}`
+                : ""
+            }`
+      }`,
+      ""
+    );
+    return data;
+  };
+  const { data, isLoading, isError, isSuccess }: any = useQuery("dev", func);
   return (
     <>
       <div className="trendings-page">
         <div className="card-wrapper">
-          <Card match={match}></Card>
+          <Card {...match}>
+            {isLoading ? (
+              <div className="loading">
+                Loading{" "}
+                {`${
+                  match.path.includes(DEVELOPERS)
+                    ? "developers"
+                    : "repositories"
+                }`}
+              </div>
+            ) : isError ? (
+              <div className="error">
+                Error Loading{" "}
+                {`${
+                  match.path.includes(DEVELOPERS)
+                    ? "developers"
+                    : "repositories"
+                }`}
+              </div>
+            ) : isSuccess ? (
+              data.map((item: any, index: any) => {
+                return <ListCardRepository {...item} key={index} />;
+              })
+            ) : (
+              ""
+            )}
+          </Card>
         </div>
       </div>
     </>
