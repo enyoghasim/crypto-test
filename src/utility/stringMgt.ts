b6f13f63eaf123361abd1252bda981754139b541
jeff-ofobrukweta <oghenerukevwejeff@gmail.com>
2021-06-12 01:35:23 +0100

    refactor: card developers

--- src/utility/stringMgt.ts
+++ src/utility/stringMgt.ts
@@ -6,18 +6,7 @@
     else return payload;
   };
   
-  // GENERATE RANDOM CHARACTERS
-  const generateRandomString = (length:number) => {
-    let chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
-    let randomstring = "";
-    for (let i = 0; i < length; i++) {
-      let rnum = Math.floor(Math.random() * chars.length);
-      randomstring += chars.substring(rnum, rnum + 1);
-    }
-    return randomstring;
-  };
   
   export {
-    getTruncatedText,
-    generateRandomString,
+    getTruncatedText
   };
